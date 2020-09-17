bfd2382309e0672fc76b065184fe44b1408eade1
Santiago Garat <sgaratnunez@gmail.com>
2020-09-17 16:23:53 -0300

    Programming Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
+#This function creates a matrix (x) and initially sets its inverse as NULL. When cacheSolve is invoked, the initial matrix inverse is set.
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
 ## Write a short comment describing this function
-
+# This function takes a matrix done within the makeCacheMatrix environment and returns the matrix inverse and sets it in the cache (if it was not there already)
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse
+  if(!is.null(inv)) {
+    message('getting cached data')
+    return (inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
